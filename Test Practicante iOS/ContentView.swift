60f0829751975c1abd2e44b052de4d4626fc1909
Edgar Alexandro Castillo Palacios <edgar_castillo@exatec.tec.mx>
2024-07-05 13:07:55 -0600

    Initial Commit

---
+++ Test Practicante iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test Practicante iOS
+//
+//  Created by Edgar Alexandro Castillo Palacios on 05/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test Practicante iOS/Test_Practicante_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Practicante_iOSApp.swift
+//  Test Practicante iOS
+//
+//  Created by Edgar Alexandro Castillo Palacios on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_Practicante_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
